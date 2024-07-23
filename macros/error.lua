bab1ed1fe949733601f0fffa97826a0f12426549
bsongis <bsongis@gmail.com>
2024-07-23 15:03:47 +0200

    Topbar updated

--- macros/error.lua
+++
@@ -1,3 +0,0 @@
-simulator.loadModel("../models/error.bin")
-simulator.pressKey(KEY_ENTER)
-simulator.screenshot("../.gitbook/assets/error.png") -- Home screen
---
+++ macros/topbar.lua
@@ -0,0 +1,3 @@
+simulator.loadModel("../models/topbar.bin")
+simulator.pressKey(KEY_ENTER)
+simulator.screenshot("../.gitbook/assets/topbar.png", {x=0, y=0, w=800, h=90}) -- Top bar
