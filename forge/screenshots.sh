c31d151dd745fc2092ad5aec782ee0173c678910
bsongis <bsongis@gmail.com>
2024-08-16 10:44:41 +0200

    Macros renamed

--- forge/screenshots.sh
+++ forge/screenshots.sh
@@ -2,8 +2,8 @@
 
 ETHOS='/mnt/c/Program Files (x86)/FrSky/Ethos'
 
-"${ETHOS}/X20S/simulator.exe"   --read-only --no-audio --no-gui --radio-settings ./x20s-fr.bin   --sd-directory ./sd --flash-directory ./flash --exec ./macros/menus.lua
+"${ETHOS}/X20S/simulator.exe"   --read-only --no-audio --no-gui --radio-settings ./x20s-fr.bin   --sd-directory ./sd --flash-directory ./flash --exec ./macros/system-menu.lua
 "${ETHOS}/X20PRO/simulator.exe" --read-only --no-audio --no-gui --radio-settings ./x20pro-fr.bin --sd-directory ./sd --flash-directory ./flash --radio-directory ./sd --exec ./macros/x20pro.lua
 "${ETHOS}/X20S/simulator.exe"   --read-only --no-audio --no-gui --radio-settings ./x20s-fr.bin --exec ./macros/mainview.lua
 "${ETHOS}/X20S/simulator.exe"   --read-only --no-audio --no-gui --radio-settings ./x20s-fr.bin --exec ./macros/user-interface.lua
-"${ETHOS}/X20S/simulator.exe"   --read-only --no-audio --no-gui --radio-settings ./x20s-fr.bin --exec ./macros/error.lua
+"${ETHOS}/X20S/simulator.exe"   --read-only --no-audio --no-gui --radio-settings ./x20s-fr.bin --exec ./macros/toolbars.lua
